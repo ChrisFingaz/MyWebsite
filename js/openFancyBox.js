eadce7b1a1c6170bb1e1aaca341273fcbb413987
Chris Larsen <chris@theblacknumbers.com>
2015-02-22 15:51:24 -0500

    Added another section, steel panther roxs

--- js/openFancyBox.js
+++ js/openFancyBox.js
@@ -76,3 +76,42 @@ $(".open_fancybox_lbi").click(function() {
     return false;
     
 });
+
+$(".open_fancybox_other").click(function() {
+    
+    $.fancybox.open([
+        {
+            href : 'img/galleries/other/steelpanther.jpg',
+            title : 'Steel Panther (band) design and flash dev'
+        },
+        {
+            href : 'img/galleries/other/verve.jpg',
+            title : 'Verve christmas album design and flash dev'
+        },
+        {
+            href : 'img/galleries/other/wackness.jpg',
+            title : 'The Wackness movie website, design'
+        },
+        {
+            href : 'img/galleries/other/entranceband.jpg',
+            title : 'Entrance Band design and dev'              
+        },
+        {
+            href : 'img/galleries/other/capella.jpg',
+            title : 'Capella luxury condos, design and flash dev'
+        },
+        {
+            href : 'img/galleries/other/waltzwithbashir.jpg',
+            title : 'Waltz With Bashir movie website, design'
+        },
+        {
+            href : 'img/galleries/other/obbp.jpg',
+            title : 'One Brooklyn Bridge Park website design and flash dev'
+        }
+    ], {
+        padding : 0
+    });
+    
+    return false;
+    
+});
