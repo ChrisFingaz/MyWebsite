e443d84c0d25504c9fcaa5a3efe0af1a2aa5dbf6
Chris Larsen <chris@theblacknumbers.com>
2015-02-22 15:10:44 -0500

    Added work exp for vimeo, etr, and lbi

--- js/openFancyBox.js
+++ js/openFancyBox.js
@@ -1,15 +1,73 @@
-$(".open_fancybox").click(function() {
+$(".open_fancybox_vimeo").click(function() {
     
     $.fancybox.open([
         {
-            href : 'img/galleries/vimeo/player.png'
+            href : 'img/galleries/vimeo/player.jpg'
         },
         {
-            href : 'img/galleries/vimeo/player.png'
+            href : 'img/galleries/vimeo/hubnut.jpg'
         },
         {
-            href : 'img/galleries/vimeo/player.png',                
-            title : '3rd title'
+            href : 'img/galleries/vimeo/outro.jpg',                
+        }
+    ], {
+        padding : 0
+    });
+    
+    return false;
+    
+});
+
+$(".open_fancybox_etr").click(function() {
+    
+    $.fancybox.open([
+        {
+            href : 'img/galleries/etr/DOND_1.jpg',
+            title : 'Deal or No Deal Slots'
+        },
+        {
+            href : 'img/galleries/etr/DOND_2.jpg',
+            title : 'Deal or No Deal Slots'
+        },
+        {
+            href : 'img/galleries/etr/DOND_3.jpg',
+            title : 'Deal or No Deal Slots'              
+        },
+        {
+            href : 'img/galleries/etr/espnRadio.jpeg',
+            title : 'ESPN Radio iOS App (iPhone and iPad)'                
+        },
+        {
+            href : 'img/galleries/etr/read180.png',
+            title : 'Scholastic Read 180'            
+        },
+        {
+            href : 'img/galleries/etr/espnMoto.jpg',
+            title : 'TG Motocross'
+
+        }
+    ], {
+        padding : 0
+    });
+    
+    return false;
+    
+});
+
+$(".open_fancybox_lbi").click(function() {
+    
+    $.fancybox.open([
+        {
+            href : 'img/galleries/lbi/BAApp.jpg',
+            title : 'Beauty Advisor AS3 Air App for Android Tablets'
+        },
+        {
+            href : 'img/galleries/lbi/Coke_Olympics.jpg',
+            title: 'Coca Cola / Olympics'
+        },
+        {
+            href : 'img/galleries/lbi/milagro.jpg',
+            title : 'Milagro / William Grant and Sons'              
         }
     ], {
         padding : 0
